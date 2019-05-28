a3430a062a398901430afa1d7ef9bd5448c996d8
hlolli <hlolli@gmail.com>
2019-05-28 17:33:03 +0200

    Contents surplis dir-path bug fixed on darwin

--- index.js
+++ index.js
@@ -25,160 +25,163 @@
 
 (function () {
 
-  const os = require('os');
-  const fs = require('fs');
-  const path = require('path');
-  const rimraf = require('rimraf');
-  const zlib = require('zlib');
-  const tar = require('tar-fs');
-  const process = require('process');
-  const request = require('request');
-  const ProgressBar = require('progress');
-  const child_process = require('child_process');
+    const os = require('os');
+    const fs = require('fs');
+    const path = require('path');
+    const rimraf = require('rimraf');
+    const zlib = require('zlib');
+    const tar = require('tar-fs');
+    const process = require('process');
+    const request = require('request');
+    const ProgressBar = require('progress');
+    const child_process = require('child_process');
 
-  const major_version = '8';
-  const update_number = '131';
-  const build_number = '11';
-  const hash = 'd54c1d3a095b4ff2b6607d096fa80163';
-  const version = major_version + 'u' + update_number;
+    const major_version = '8';
+    const update_number = '131';
+    const build_number = '11';
+    const hash = 'd54c1d3a095b4ff2b6607d096fa80163';
+    const version = major_version + 'u' + update_number;
     const jreDirName = exports.jreDirName = 'jre' + '1' + '.' + major_version +
                                             '.' + '0' + '_' + update_number +
-                                            (process.platform === 'darwin' ? '.jre' : '');
+                                             (process.platform === 'darwin' ? '.jre' : '');
 
-  var _jreDir;
-  const jreDir = exports.jreDir = () => {
-    if (_jreDir) {
-      return _jreDir;
+    var _jreDir;
+    const jreDir = exports.jreDir = () => {
+        if (_jreDir) {
+            return _jreDir;
+        }
+        return path.join(__dirname, 'jre');
+    };
+    const setJreDir = exports.setJreDir = dir => _jreDir = dir;
+
+    const fail = reason => {
+        console.error(reason);
+        process.exit(1);
+    };
+
+    var _arch = os.arch();
+    switch (_arch) {
+        case 'x64':
+            break;
+        case 'ia32':
+            _arch = 'i586';
+            break;
+        default:
+            fail('unsupported architecture: ' + _arch);
     }
-    return path.join(__dirname, 'jre');
-  };
-  const setJreDir = exports.setJreDir = dir => _jreDir = dir;
+    const arch = exports.arch = () => _arch;
+    const setArch = exports.setArch = newArch => _arch = newArch;
 
-  const fail = reason => {
-    console.error(reason);
-    process.exit(1);
-  };
+    var _platform = os.platform();
+    var _driver;
+    switch (_platform) {
+        case 'darwin':
+            _platform = 'macosx';
+            _driver = ['Contents', 'Home', 'bin', 'java'];
+            break;
+        case 'win32':
+            _platform = 'windows';
+            _driver = ['bin', 'javaw.exe'];
+            break;
+        case 'linux':
+            _driver = ['bin', 'java'];
+            break;
+        default:
+            fail('unsupported platform: ' + _platform);
+    }
+    const platform = exports.platform = () => _platform;
 
-  var _arch = os.arch();
-  switch (_arch) {
-    case 'x64':
-      break;
-    case 'ia32':
-      _arch = 'i586';
-      break;
-    default:
-      fail('unsupported architecture: ' + _arch);
-  }
-  const arch = exports.arch = () => _arch;
-  const setArch = exports.setArch = newArch => _arch = newArch;
+    const getDirectories = dirPath => fs.readdirSync(dirPath).filter(
+        file => fs.statSync(path.join(dirPath, file)).isDirectory()
+    );
 
-  var _platform = os.platform();
-  var _driver;
-  switch (_platform) {
-    case 'darwin':
-      _platform = 'macosx';
-      _driver = ['Home', 'bin', 'java'];
-      break;
-    case 'win32':
-      _platform = 'windows';
-      _driver = ['bin', 'javaw.exe'];
-      break;
-    case 'linux':
-      _driver = ['bin', 'java'];
-      break;
-    default:
-      fail('unsupported platform: ' + _platform);
-  }
-  const platform = exports.platform = () => _platform;
+    const driver = exports.driver = () => {
+        var jreDirs = getDirectories(jreDir());
+        if (jreDirs.length < 1)
+            fail('no jre found in archive');
+        var d = _driver.slice();
+        if ((_platform !== 'darwin') &&
+            (_platform !== 'macosx')) {
+            d.unshift(jreDirs[0]);
+        }
+        d.unshift(jreDir());
+        return path.join.apply(path, d);
+    };
 
-  const getDirectories = dirPath => fs.readdirSync(dirPath).filter(
-    file => fs.statSync(path.join(dirPath, file)).isDirectory()
-  );
+    const getArgs = exports.getArgs = (jvm_args, classname, args) => {
+        args = (args || []).slice();
+        jvm_args = jvm_args || [];
+        args.unshift(classname);
+        args.unshift(jvm_args.join(" "));
+        args.unshift('-cp');
+        return args;
+    };
 
-  const driver = exports.driver = () => {
-    var jreDirs = getDirectories(jreDir());
-    if (jreDirs.length < 1)
-      fail('no jre found in archive');
-    var d = _driver.slice();
-    d.unshift(jreDirs[0]);
-    d.unshift(jreDir());
-    return path.join.apply(path, d);
-  };
+    const spawn = exports.spawn =
+        (classpath, classname, args, options) =>
+            child_process.spawn(driver(), getArgs(classpath, classname, args), options);
 
-  const getArgs = exports.getArgs = (jvm_args, classname, args) => {
-    args = (args || []).slice();
-    jvm_args = jvm_args || [];
-    args.unshift(classname);
-    args.unshift(jvm_args.join(" "));
-    args.unshift('-cp');
-    return args;
-  };
+    const spawnSync = exports.spawnSync =
+        (classpath, classname, args, options) =>
+            child_process.spawnSync(driver(), getArgs(classpath, classname, args), options);
 
-  const spawn = exports.spawn =
-    (classpath, classname, args, options) =>
-      child_process.spawn(driver(), getArgs(classpath, classname, args), options);
+    const smoketest = exports.smoketest = () =>
+        spawnSync(['resources'], 'Smoketest', [], {encoding: 'utf8'})
+            .stdout.trim() === 'No smoke!';
 
-  const spawnSync = exports.spawnSync =
-    (classpath, classname, args, options) =>
-      child_process.spawnSync(driver(), getArgs(classpath, classname, args), options);
+    const url = exports.url = () =>
+        'https://download.oracle.com/otn-pub/java/jdk/' +
+                                  version + '-b' + build_number + '/' + hash +
+                                  '/jre-' + version + '-' + platform() + '-' + arch() + '.tar.gz';
 
-  const smoketest = exports.smoketest = () =>
-    spawnSync(['resources'], 'Smoketest', [], {encoding: 'utf8'})
-      .stdout.trim() === 'No smoke!';
-
-  const url = exports.url = () =>
-    'https://download.oracle.com/otn-pub/java/jdk/' +
-    version + '-b' + build_number + '/' + hash +
-    '/jre-' + version + '-' + platform() + '-' + arch() + '.tar.gz';
-
-  const install = exports.install = callback => {
-    var urlStr = url();
-    console.log("Downloading from: ", urlStr);
-    callback = callback || (() => {
-    });
-    rimraf(jreDir(), function () {
-      request
-        .get({
-          url: url(),
-          rejectUnauthorized: false,
-          agent: false,
-          headers: {
-            connection: 'keep-alive',
-            'Cookie': 'gpw_e24=http://www.oracle.com/; oraclelicense=accept-securebackup-cookie'
-          }
-        })
-        .on('response', res => {
-          var len = parseInt(res.headers['content-length'], 10);
-          var bar = new ProgressBar('  downloading and preparing JRE [:bar] :percent :etas', {
-            complete: '=',
-            incomplete: ' ',
-            width: 80,
-            total: len
-          });
-          res.on('data', chunk => bar.tick(chunk.length));
-        })
-        .on('error', err => {
-          console.log(`problem with request: ${err.message}`);
-          callback(err);
-        })
-        .on('end', () => {
-          try {
-            if (smoketest()) callback(); else callback("Smoketest failed.");
-          } catch (err) {
-            callback(err);
-          }
-        })
-        .pipe(zlib.createUnzip())
-        .on('error', err => {
-          console.log(`problem with unzipping the JRE archive: ${err.message}`);
-          callback(err);
-        })
-        .pipe(tar.extract(jreDir()))
-        .on('error', err => {
-          console.log(`problem with extracting files: ${err.message}`);
-          callback(err);
+    const install = exports.install = callback => {
+        var urlStr = url();
+        console.log("Downloading from: ", urlStr);
+        callback = callback || (() => {
         });
-    });
-  };
+        rimraf(jreDir(), function () {
+            request
+                .get({
+                    url: url(),
+                    rejectUnauthorized: false,
+                    agent: false,
+                    headers: {
+                        connection: 'keep-alive',
+                        'Cookie': 'gpw_e24=http://www.oracle.com/; oraclelicense=accept-securebackup-cookie'
+                    }
+                })
+                .on('response', res => {
+                    var len = parseInt(res.headers['content-length'], 10);
+                    var bar = new ProgressBar('  downloading and preparing JRE [:bar] :percent :etas', {
+                        complete: '=',
+                        incomplete: ' ',
+                        width: 80,
+                        total: len
+                    });
+                    res.on('data', chunk => bar.tick(chunk.length));
+                })
+                .on('error', err => {
+                    console.log(`problem with request: ${err.message}`);
+                    callback(err);
+                })
+                .on('end', () => {
+                    try {
+                        if (smoketest()) callback(); else callback("Smoketest failed.");
+                    } catch (err) {
+                        callback(err);
+                    }
+                })
+                .pipe(zlib.createUnzip())
+                .on('error', err => {
+                    console.log(`problem with unzipping the JRE archive: ${err.message}`);
+                    callback(err);
+                })
+                .pipe(tar.extract(jreDir()))
+                .on('error', err => {
+                    console.log(`problem with extracting files: ${err.message}`);
+                    callback(err);
+                });
+        });
+    };
 
 })();
